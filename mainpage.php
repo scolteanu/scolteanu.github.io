5e44cab83d435f8e8c15532ada59033122e5d8c3
scolteanu <40582643+scolteanu@users.noreply.github.com>
2018-07-23 23:48:31 +0300

    Add files via upload

---
+++ mainpage.php
@@ -0,0 +1,147 @@
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<!--	<base href="mainpage.php">    
+-->
+    <link rel="shortcut icon" href="favicon.ico" >
+    <title>ACD 2017, Bucharest</title>
+    <meta name="keywords" content="14th, International, Workshop, Advanced, Control, Diagnosis, ACD, 2017, ACD2017, home, page, Bucharest. " />
+    <meta name="description" content="14th International Workshop on Advanced Control and Diagnosis - Home page" />
+
+    <link href="default.css" rel="stylesheet" type="text/css" />
+    <script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+    <script src="unslider.min.js"></script>
+
+    <script>
+        $(function () {
+            $('.slideshow').unslider({
+                speed: 1000,
+                delay: 5000,
+                dots: true
+            });
+        });
+        /*
+         $(function() {
+         $("#slideshow > div:gt(0)").hide();
+         setInterval(function() { 
+         $('#slideshow > div:first')
+         .fadeOut(1000)
+         .next()
+         .fadeIn(1000)
+         .end()
+         .appendTo('#slideshow');
+         },  3000);	
+         });
+         */
+    </script>
+
+    <script>
+          function toHome()
+           {   $("#diffpages").load("home.html");         }
+          function toScope()
+           {   $("#diffpages").load("scopes.html");       }
+          function toProgramme()
+           {   $("#diffpages").load("programme.html");    }
+          function toPlenary()
+           {   $("#diffpages").load("plenary.html");      }
+          function toCommittees()
+           {   $("#diffpages").load("committees.html");   }
+          function toPast()
+           {   $("#diffpages").load("past.html");         }
+          function toCfp()
+           {   $("#diffpages").load("cfp.html");          }
+          function toIdates()
+           {   $("#diffpages").load("idates.html");        }
+          function toRegistration()
+           {   $("#diffpages").load("registration.html"); }
+          function toLocation()
+           {   $("#diffpages").load("location.html");     }
+          function toTravel()
+           {   $("#diffpages").load("travel.html");       }
+          function toHotels()
+           {   $("#diffpages").load("hotels.html");       }
+          function toVenue()
+           {   $("#diffpages").load("venue.html");        }
+          function toContact()
+           {   $("#diffpages").load("contact.html");      }
+     </script>
+
+
+</head>
+
+
+
+<body >
+    <div id="page">
+        <div id="sidebar">
+            <div id="logo">
+                <div id="logoIm"> <a href="#"><img src="images/acd_logo2017.png" width="169" height="169" /></a></div>
+            </div>
+            <!-- end header -->
+            <div id="menu">
+                <ul>
+                    <li class="first"><a href="javascript:toHome();">Home</a></li>
+                    <li><a href="javascript:toScope();">Scopes and Objectives</a></li>
+                    <li><a href="javascript:toProgramme();">Programme</a></li>
+                    <li><a href="javascript:toPlenary();">Plenary Lectures</a></li>                
+                    <li><a href="javascript:toCommittees();">Committees</a></li>
+                    <li><a href="javascript:toPast();">Past Conferences</a></li>
+                    <li><a href="javascript:toCfp();">Paper Submission</a></li>
+                    <li><a href="javascript:toIdates();">Important Dates</a></li>
+                    <li><a href="javascript:toRegistration();">Registration</a></li> 
+                    <li><a href="javascript:toLocation();">About Bucharest</a></li>
+                    <li><a href="javascript:toTravel();">Travel Information</a></li>                                                                                              
+                    <li><a href="javascript:toHotels();">List of Hotels</a></li>
+                    <li><a href="javascript:toVenue();">Venue</a></li>  
+                    <li><a href="javascript:toContact();">Contact</a></li>
+                </ul>
+            </div>
+            <!-- end menu -->
+        </div>
+        <!-- end sidebar -->
+        <div id="content">
+            <div class="slideshow">
+                <ul>
+                    <li>  <img src="images/rectorat.png" width="740" height="300" /></a></li>
+                    <li>  <img src="images/Automatica.png" width="740" height="300" /></a></li>
+                    <li>  <img src="images/biblioteca.png" width="740" height="300" /></a></li>
+                    <li>  <img src="images/bucuresti_seara.png" width="740" height="300" /></a></li>
+                    <li>  <img src="images/casapopor.png" width="740" height="300" /></a></li>
+                    <li>  <img src="images/Dambovita.png" width="740" height="300" /></a></li>
+                    <li>  <img src="images/bucuresti_overview.png" width="740" height="300" /></a></li>
+                </ul>
+            </div>
+            <div class="boxed"  id="diffpages">
+                 <script> $("#diffpages").load("home.html"); </script>
+            </div>
+        </div>
+        <!-- end content -->
+        <div style="clear: both;"></div>
+    </div>
+    <!-- end page -->
+    <div id="footer" >
+<!--
+        <p id="sponsor">  
+
+                          <a href="http://www.hei.fr"><img src="images/IJHE Logo.png" style="padding: 0px 0px 0px 80px; height: 55px">
+                          <br>
+                          <a href="http://www.hei.fr"><img src="images/SIC_logo.png" style="padding: 0px 0px 0px 0px; height: 45px"> 
+        </p>
+-->
+        <p id="logos" style="display:inline">  
+<!--
+                          <a href="http://www.upb.ro/en/" ><img src="images/ifac_logo.png" style="padding: 0px 0px 0px 100px; height: 65px">
+-->
+                          <a href="https://sic.ici.ro/"><img src="images/SIC_logo.png" style="padding: 0px 0px 0px 0px; height: 50px">
+                          <a href="http://www.upb.ro/en/" ><img src="images/politehnica_logo.png" style="padding: 0px 0px 0px 200px; height: 65px">
+                          <a href="http://acs.pub.ro/en/" ><img src="images/automatica_logo.png" style="padding: 0px 0px 0px 70px; height: 65px"> 
+                          <br> 
+                          <a href="http://www.hei.fr"><img src="images/IAHE_IJHE_logo.png" style="padding: 0px 0px 0px 80px; height: 55px">
+                          <a href="https://www.cristal.univ-lille.fr" ><img src="images/cristal_logo.png" style="padding: 0px 0px 0px 60px; height: 65px">                         
+                          <a href="http://www.aosr.ro/en/" ><img src="images/AOSR_logo.png" style="padding: 0px 0px 0px 70px; height: 65px">                         
+                          <a href="http://www.astr.ro/" ><img src="images/astr_logo.png" style="padding: 0px 0px 0px 150px; height: 65px">                         
+        </p>
+
+    </div>
+</body>
+
+		
